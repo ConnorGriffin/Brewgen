43fcf154c1c84fe1615f844028f21991cb45d21d
Connor Griffin <connor@connorcg.com>
2019-09-30 22:30:54 -0700

    Move sensory data calculations to GrainModel

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import json
+from ortools.sat.python import cp_model
 
 class Category:
     """Defines a category and all of its properties."""
@@ -36,7 +37,7 @@ class CategoryModel:
 
 
     def get_category_list(self):
-        """Return all categories as a list of dicts"""
+        """Return all categories as a list of dicts."""
         return [category.get_category_data() for category in self.category_list]
 
 
@@ -75,7 +76,7 @@ class Grain:
 
 
 class GrainModel:
-    """Defines a Grain Model, used to access data about grains in the grain database"""
+    """Defines a Grain Model, used to access data about grains in the grain database."""
 
     def __init__(self):
         self.grain_list = []
@@ -98,12 +99,12 @@ class GrainModel:
 
 
     def get_grain_list(self):
-        """Return a list of all grains as a list of dicts"""
+        """Return a list of all grains as a list of dicts."""
         return [grain.get_grain_data() for grain in self.grain_list]
 
 
     def get_grain_by_category(self, category):
-        """Return a list of grain objects belonging to the specified categories"""
+        """Return a list of grain objects belonging to the specified categories."""
         return [grain for grain in self.grain_list if grain.category in category]
 
 
@@ -112,13 +113,13 @@ class GrainModel:
 
 
     def get_all_categories(self):
-        """Return a list of unique grain categories, optionally filtered on a list of grains"""
+        """Return a list of unique grain categories, optionally filtered on a list of grains."""
         # TODO: Complete this. Not sure if grain_list should be a list of objects or a list of slugs.
         pass
 
 
     def get_sensory_keywords(self):
-        """Return a list of unique sensory keywords, optionally filtered on a list of grains"""
+        """Return a list of unique sensory keywords, optionally filtered on a list of grains."""
         # TODO: Add the filtering part, same as above, not sure if filtering on slugs or what
         sensory_keys = []
         for grain in self.grain_list:
@@ -134,8 +135,92 @@ class GrainModel:
         return [grain.slug for grain in self.grain_list]
 
 
-    def get_sensory_data(self):
-        pass
+    def get_sensory_data(self, max_unique_grains=4, category_model=CategoryModel()):
+        """Return minimum and maximum sensory data based on grain list and category data."""
+        scaling = 100
+
+        # Init the category, grain, and sensory data
+        category_data = category_model.get_category_list()
+        grain_data = self.get_grain_list()
+        sensory_keys = self.get_sensory_keywords()
+
+        # Set the ranges, we'll need to index into things a lot
+        grain_range = range(len(grain_data))
+        category_range = range(len(category_data))
+        sensory_range = range(len(sensory_keys))
+
+        # Build a list of all grains and their sensory data
+        grain_map = []
+        for grain in grain_data:
+            # Build a list of sensory data, add every possible keyword, set to zero if not mapped already
+            sensory_data = {}
+            for key in sensory_keys:
+                sensory_data[key] = int(grain['sensory_data'].get(key, 0) * scaling)
+
+            grain_map.append({
+                'slug': grain['slug'],
+                'category': grain['category'],
+                'max_percent': grain['max_percent'],
+                'sensory_data': sensory_data
+            })
+
+        # Create the model
+        model = cp_model.CpModel()
+
+        # Define model variables
+        grain_vars = [model.NewIntVar(0, 100, 'grain{}'.format(i)) for i in grain_range]
+        category_vars = [model.NewIntVar(0, 100, 'category_{}'.format(category['name'])) for category in category_data]
+        sensory_vars = [model.NewIntVar(0, 1000, 'sensory_{}'.format(key)) for key in sensory_keys]
+        grain_used = [model.NewBoolVar('grain{}_used'.format(i)) for i in grain_map]
+
+        # Define constraints
+        # Grain usage total must be 100% - not sure why both of these are needed but it doens't work if they're not
+        model.Add(sum(grain_vars) == 100)
+        model.Add(sum(category_vars) == 100)
+
+        # Limit the max number of grains to the specified limit
+        for i in grain_range:
+            model.Add(grain_vars[i] == 0).OnlyEnforceIf(grain_used[i].Not())
+            model.Add(grain_vars[i] > 0).OnlyEnforceIf(grain_used[i])
+        model.Add(sum(grain_used) <= max_unique_grains)
+
+        # Keep each grain under the max amount
+        for i in grain_range:
+            model.Add(grain_vars[i] <= grain_map[i]['max_percent'])
+
+
+        # Keep each grain category at or above the min and at or below the max amounts
+        for i in category_range:
+            category = category_data[i]
+            model.Add(category_vars[i] == sum(grain_vars[k] for k in grain_range if grain_map[k]['category'] == category['name']))
+
+            model.Add(category_vars[i] <= category['max_percent'])
+            model.Add(category_vars[i] >= category['min_percent'])
+
+        # Assign the sensory values
+        for i in sensory_range:
+            sensory_key = sensory_keys[i]
+            # Assign sensory var = Sum of all grains (sensory key * usage percent)
+            model.Add(sensory_vars[i] * 100 == sum(grain_map[k]['sensory_data'][sensory_key] * grain_vars[k] for k in grain_range))
+
+        # Solve for minimizing and maximizing each descriptor
+        solver = cp_model.CpSolver()
+        sensory_data = {}
+        for i in sensory_range:
+            model.Maximize(sensory_vars[i])
+            status = solver.Solve(model)
+            maximum = solver.Value(sensory_vars[i])
+
+            model.Minimize(sensory_vars[i])
+            status = solver.Solve(model)
+            minimum = solver.Value(sensory_vars[i])
+
+            sensory_data[sensory_keys[i]] = {
+                "min": minimum / scaling,
+                "max": maximum / scaling
+            }
+
+        return sensory_data
 
 
 class GrainList(GrainModel):
@@ -150,8 +235,13 @@ class GrainList(GrainModel):
 
 
     def add_grain(self, grain):
-        """Adds a grain to the grain bill object
+        """Adds a grain to the grain bill object.
         Args:
             grain (Grain()): The grain to add, can be an instance of the Grain() class or a valid grain slug
         """
         self.grain_list.append(grain)
+
+
+grain_model = GrainModel()
+grain_list = GrainList(grain_model.get_grain_slugs())
+print(grain_list.get_sensory_data())
